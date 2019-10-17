1e372eaee413db024acffb67549601adceb7fe1d
Zhiming <zhiming.zhong@outlook.com>
2019-10-17 17:20:52 +0800

    add model article; add resource article; add controller articles; add db/migration

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,31 @@
+class ArticlesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+end
