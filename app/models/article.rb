c8d95d28806a8cb0559a96100088657a9305b78e
Zhiming <zhiming.zhong@outlook.com>
2019-10-18 10:00:23 +0800

    add column status to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ApplicationRecord
+  scope :available, -> { where(status: true) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_090331) do
+ActiveRecord::Schema.define(version: 2019_10_18_014916) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "status", default: true
   end
 
 end
