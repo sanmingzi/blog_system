d01a663249713d97ec16572a589fb6d8e97d1cb5
Zhiming <zhiming.zhong@outlook.com>
2019-10-18 09:44:21 +0800

    add new/show/index of articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def new
@@ -12,6 +13,7 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def edit
